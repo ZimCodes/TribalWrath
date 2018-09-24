f6ed9415c66cf6f543a4e73954168e6335d36a60
Lavance Strickland <lavance_strickland@yahoo.com>
2018-09-24 00:33:26 -0500

    Added the ability to Jump higher

--- TribalWrath/Assets/Scripts/Abilities/Abilities.cs
+++ TribalWrath/Assets/Scripts/Abilities/Abilities.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum AbilityState { HighJump };
+public enum AbilityState { HighJump,AcidSpit };
 
 public class Abilities : IAbilities
 {
--- TribalWrath/Assets/Scripts/Controllers/AbilitiesController.cs
+++ TribalWrath/Assets/Scripts/Controllers/AbilitiesController.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AbilitiesController : MonoBehaviour {
-
+    private AbilityState ability = AbilityState.HighJump;
     [Tooltip("Go to the Unity Documentation for more details")]
     public ForceMode forceMode = ForceMode.Force;
+    public ForceMode highJumpForceMode = ForceMode.Force;
     Jumping jump;
 
     [Tooltip("Amount of Force Required to Jump")]
     public float jumpForce = 12;
+    public float highJumpForce = 12;
 
     [Tooltip("Maximum Height to jump before descending")]
     public float jumpHeight = 5;
-
+    public float highJumpHeight = 5;
     // Use this for initialization
     void Start()
     {
@@ -24,19 +26,10 @@ public class AbilitiesController : MonoBehaviour {
     // Update is called once per frame
     void FixedUpdate()
     {
-        //if (KeyboardInputUtil.KeyWasPressed(KeyCode.Alpha1))
-        //{
-        //    UpdateJumpInput();
+        InputSwitchAbilities();
 
-        //    //For Designers
-        //    jump.jumpPower = jumpForce;
-        //    jump.jumpHeight = jumpHeight;
-
-        //    jump.UpdateJumpMovement(forceMode);
-        //    jump.MaximumjumpHeightCheck(this.gameObject);
-        //}
-        
     }
+    
     private void UpdateJumpInput()
     {
         if (KeyboardInputUtil.KeyWasPressed(KeyCode.Space) && !jump.isJumping)
@@ -47,7 +40,40 @@ public class AbilitiesController : MonoBehaviour {
         }
         jump.Direction.Normalize();
     }
+    private void InputSwitchAbilities()
+    {
+        if (KeyboardInputUtil.KeyWasPressed(KeyCode.Alpha1))
+        {
+            ability = AbilityState.HighJump;
+        }
+        AbilitySwitch();
+    }
+    private void AbilitySwitch()
+    {
+        switch (ability)
+        {
+            case AbilityState.HighJump:
+                UpdateJumpInput();
 
+                //For Designers
+                jump.jumpPower = highJumpForce;
+                jump.jumpHeight = highJumpHeight;
+                jump.UpdateJumpMovement(forceMode);
+                break;
+            case AbilityState.AcidSpit:
+                UpdateJumpInput();
+
+                //For Designers
+                jump.jumpPower = jumpForce;
+                jump.jumpHeight = jumpHeight;
+                jump.UpdateJumpMovement(highJumpForceMode);
+                break;
+            default:
+                ability = AbilityState.HighJump;
+                break;
+        }
+        jump.MaximumjumpHeightCheck(this.gameObject);
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.contacts.Length > 0)
