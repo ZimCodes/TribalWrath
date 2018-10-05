6a77b7edba2432f733f54ba2e9dbff609b03d187
ColeMcdaniel <32074076+ColeMcdaniel@users.noreply.github.com>
2018-10-04 22:32:50 -0500

    Spit ability
    spit ability

---
+++ TribalWrath/Assets/Scripts/AcidSpit/AcidSpit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcidSpit : MonoBehaviour {
+
+	GameObject prefab;
+
+	private float cooldown = 0;
+	private const float ShootInterval = 3f;
+
+	void Start () {
+		prefab = Resources.Load ("Spit") as GameObject;
+	}
+
+	void Shoot()
+	{
+		if(cooldown > 1)
+			return;
+
+		// shoot bullet
+		cooldown = ShootInterval;
+	}
+
+	void Update () {
+		
+		cooldown -= Time.deltaTime;
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			GameObject spit = Instantiate (prefab) as GameObject;
+			spit.transform.position = transform.position + Camera.main.transform.forward * 1;
+			Rigidbody rb = spit.GetComponent<Rigidbody> ();
+			rb.velocity = Camera.main.transform.forward * 10;
+
+		}
+	}
+}
---
+++ TribalWrath/Assets/Scripts/Crosshair/Crosshair.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour {
+
+	[SerializeField]Texture2D image;
+	[SerializeField]int size;
+	[SerializeField]float maxAngle;
+	[SerializeField]float minAngle;
+
+	float lookHeight;
+
+	public void Lookheight(float value){
+		lookHeight += value;
+
+		if(lookHeight > maxAngle || lookHeight < minAngle)
+			lookHeight -= value;
+	}
+
+//	void OnGUI() {
+//		Vector3 screenPosition = Camera.mainWorldToScreenPoint (transform.position);
+//		screenPostion.y = Screenheight - screenPosition.y;
+//		GUI.DrawTexture (new Rect (screenPosition.x, screenPostion.y - lookHeight, size, size), image);
+//	}
+}
+	
