a2b529d84ec978e8aadb81d75a4715f620b7e283
Lavance Strickland <lavance_strickland@yahoo.com>
2018-10-22 17:09:29 -0500

    Added Mouse Ability.

---
+++ TribalWrath/Assets/Scripts/Programmers/MouseShrink.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseShrink : Abilities {
+
+    private Vector3 defaultScale, shrinkScale,defaultPosition,currentPosition;
+    private GameObject gameObject;
+    public MouseShrink(GameObject _gameobj,float _xscale,float _yscale,float _zscale)
+    {
+        this.gameObject = _gameobj;
+        this.shrinkScale = new Vector3(_xscale,_yscale,_zscale);
+    }
+    public MouseShrink(GameObject _gameobj, Vector3 _shrinkvect)
+    {
+        this.gameObject = _gameobj;
+        this.shrinkScale = _shrinkvect;
+    }
+	// Use this for initialization
+	public void Start () {
+        this.defaultScale = this.gameObject.transform.localScale;
+        this.defaultPosition = this.gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+        //TODO: Adjust it for abilities and not its own independent inputs
+        MouseShrinkInput();
+    }
+    private void MouseShrinkInput()
+    {
+        if (KeyboardInputUtil.KeyWasPressed(KeyCode.Keypad1))
+        {
+            this.gameObject.transform.localScale = this.shrinkScale;
+        }
+        else if (KeyboardInputUtil.KeyWasPressed(KeyCode.Keypad2))
+        {
+            currentPosition = this.gameObject.transform.position;
+            if (currentPosition.y < defaultPosition.y)
+            {
+                this.gameObject.transform.position = this.defaultPosition;
+            }
+            this.gameObject.transform.localScale = this.defaultScale;
+        }
+    }
+    public void ChangeDefaultPosition(Vector3 _objposition)
+    {
+        defaultPosition = _objposition;
+        Debug.Log(defaultPosition);
+    }
+    
+}
---
+++ TribalWrath/Assets/Scripts/Programmers/MouseShrinkController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseShrinkController : MonoBehaviour {
+    MouseShrink shrink;
+    [Tooltip("The new scale for the gameobject.[No Runtime Changes]")]
+    public Vector3 shrinkVector = new Vector3(0.25f,0.25f,0.25f);
+	// Use this for initialization
+	void Start () {
+        shrink = new MouseShrink(this.gameObject,shrinkVector);
+        shrink.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        shrink.Update();
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.contacts.Length > 0)
+        {
+            shrink.ChangeDefaultPosition(this.transform.position);
+        }
+    }
+}
